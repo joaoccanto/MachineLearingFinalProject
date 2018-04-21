18b31e5b693f866544561d14d7f74bf9a334900c
joaoccanto <joaocarlos_almeida@student.uml.edu>
2018-04-21 10:36:08 -0400

    improving the code, and creating result file

--- LogisticRegressionCrimeData.py
+++ LogisticRegressionCrimeData.py
@@ -1,6 +1,9 @@
 import numpy as np
+import sys
+
 
 class LR(object):
+
     def __init__(self, eta=0.1, conv=.0001):
         self.eta = eta
         self.conv = conv
@@ -37,7 +40,7 @@ class LR(object):
             self.w -= (self.eta * dw.T)
             cost = self.cost_function(data, label)
             change_cost = old_cost - cost
-            print cost
+            #print cost
             i = i +1
         print i, change_cost
         return
@@ -54,29 +57,75 @@ class LR(object):
         
 
 def main():
+
+    sys.stdout = open('file', 'w')
+
     data_file = 'data_set.txt'
-    data_set, label = load_data_set(data_file)
+    data_set, label, cities = load_data_set(data_file)
 
-    data_reduced_sample_space, label_reduced_sample_space = decrease_sample_size(data_set, label)
-
-    data_reduced_feature_space = decrease_feature_space(data_set)
+    data_reduced_sample_space, label_reduced_sample_space, cities_reduced_samples = decrease_sample_size(data_set, label,cities)
 
     new_label = lr_label(label_reduced_sample_space)
 
+    tr_data, tr_label, tr_city = training_sets(data_reduced_sample_space, new_label, cities_reduced_samples)
+    t_data, t_label, t_cities = test_sets(data_reduced_sample_space, new_label, cities_reduced_samples)
+
     logistic_red_sample_size = LR()
-    logistic_red_sample_size.init_weights(len(data_reduced_sample_space[0]))
-    logistic_red_sample_size.grad_descent(data_reduced_sample_space, new_label)
-    predictions = logistic_red_sample_size.predict(data_reduced_sample_space)
-    num_of_data = len(data_reduced_sample_space)
-    
-    #logistic_reduced_features = LR()
-    #logistic_reduced_features.init_weights(len(data_reduced_feature_space[0]))
-    #logistic_reduced_features.grad_descent(data_reduced_feature_space, new_label)
-    #predictions = logistic_reduced_features.predict(data_reduced_feature_space)
-    #num_of_data = len(data_reduced_feature_space)
+    logistic_red_sample_size.init_weights(len(tr_data[0]))
+    logistic_red_sample_size.grad_descent(tr_data, tr_label)
+    predictions = logistic_red_sample_size.predict(t_data)
+    num_of_data = len(t_data)
 
     count = 0
-    for p, l in zip(predictions.T, new_label):
+
+    print "reduced sample space"
+    for p, l, c in zip(predictions.T, t_label, t_cities):
+        print c , "\t" ,p , "\t", l
+        if p == l:
+            count = count + 1
+    print count, num_of_data
+###########################################################
+    data_reduced_feature_space = decrease_feature_space(data_set)
+    new_label = lr_label(label)
+
+    tr_data, tr_label, tr_city = training_sets(data_reduced_feature_space, new_label, cities)
+    t_data, t_label, t_city = test_sets(data_reduced_feature_space, new_label, cities)
+
+    
+    logistic_red_sample_size = LR()
+    logistic_red_sample_size.init_weights(len(tr_data[0]))
+    logistic_red_sample_size.grad_descent(tr_data, tr_label)
+    predictions = logistic_red_sample_size.predict(t_data)
+    num_of_data = len(t_data)
+
+    count = 0
+
+    print "reduced feature space"
+    for p, l, c in zip(predictions.T, t_label, t_city):
+        print c , "\t" ,p , "\t", l
+        if p == l:
+            count = count + 1
+    print count, num_of_data
+
+    ############################
+    double_reduced_data= decrease_feature_space(data_reduced_feature_space)
+    double_reduced_data, double_reduced_label, double_reduced_cities= decrease_sample_size(double_reduced_data, label, cities)
+    new_label = lr_label(double_reduced_label)
+
+    tr_data, tr_label, tr_city = training_sets(double_reduced_data, new_label, double_reduced_cities)
+    t_data, t_label, t_city = test_sets(double_reduced_data, new_label, double_reduced_cities)
+
+    logistic_red_sample_size = LR()
+    logistic_red_sample_size.init_weights(len(tr_data[0]))
+    logistic_red_sample_size.grad_descent(tr_data, tr_label)
+    predictions = logistic_red_sample_size.predict(t_data)
+    num_of_data = len(t_data)
+
+    count = 0
+
+    print "reduced sample and feature space"
+    for p, l, c in zip(predictions.T, t_label, t_city):
+        print c , "\t" ,p , "\t", l
         if p == l:
             count = count + 1
     print count, num_of_data
@@ -84,11 +133,14 @@ def main():
 def load_data_set(data_file):
     data_set = []
     label = []
+    cities = []
     with open(data_file, 'r') as f:
         for line in f:
             row = np.ones(1)
             sline = line.replace('\n', '')
             t = sline.split(',')
+            #load city
+            cities.append(t[3])
             #load data set
             # first five elements are not predictive and the last element is the label
             for i in range(5, 126):
@@ -99,17 +151,19 @@ def load_data_set(data_file):
                     row = np.append(row, [float(t[i])])
             data_set.append(row)
             label.append(float(t[127]))
-    return np.asarray(data_set), np.asarray(label)
+    return np.asarray(data_set), np.asarray(label), cities
 
 # remove any sample set that is missing a feature
-def decrease_sample_size(data, label):
+def decrease_sample_size(data, label, cities):
     reduced_data = []
     reduced_label = []
-    for row, lbl in zip(data, label):
+    reduced_cities = []
+    for row, lbl, c in zip(data, label, cities):
         if not ([-1] in row):
             reduced_data.append(row)
             reduced_label.append(lbl)
-    return np.asarray(reduced_data), np.asarray(reduced_label)
+            reduced_cities.append(c)
+    return np.asarray(reduced_data), np.asarray(reduced_label), reduced_cities
 
 def decrease_feature_space(data):
     num_of_samples = (len(data))
@@ -134,11 +188,36 @@ def decrease_feature_space(data):
 
 def lr_label(label):
     new_label = []
+    threshold = np.mean(label)
     #if the crime rate is greater than .30, then label the community not safe
     for l in label:
-        if l > .20:
+        if l > threshold:
             new_label.append([0])
         else:
             new_label.append([1])
     return np.asarray(new_label)
+
+def training_sets(data, label, city):
+    t_data = []
+    t_label = []
+    t_cities = []
+    num_of_data = len(data)
+
+    for i in range(0, (num_of_data - num_of_data / 10)):
+        t_data.append(data[i])
+        t_label.append(label[i])
+        t_cities.append(city[i])
+    return np.asarray(t_data), np.asarray(t_label), t_cities
+
+def test_sets(data, label, city):
+    t_data = []
+    t_label = []
+    t_cities = []
+    num_of_data = len(data)
+    for i in range(num_of_data - num_of_data / 10, num_of_data):
+        t_data.append(data[i])
+        t_label.append(label[i])
+        t_cities.append(city[i])
+    return np.asarray(t_data), np.asarray(t_label), t_cities
+
 main()
